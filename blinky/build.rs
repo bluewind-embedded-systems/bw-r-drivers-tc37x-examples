a45cdc8b6a7a897a12726e08d803a97be29e3a73
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-03-25 12:28:23 +0100

    Add target_arch check

--- blinky/build.rs
+++ blinky/build.rs
@@ -1,15 +1,29 @@
 use std::{env, error::Error, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ld_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap())
-        .join("ld")
-        .canonicalize()?;
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
 
-    println!("cargo:rustc-link-search={}", ld_dir.display());
+    if target_arch != "tricore" {
+        panic!("Only tricore target is supported")
+    }
 
+    // Linker script directory
+    {
+        let ld_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap())
+            .join("ld")
+            .canonicalize()?;
+
+        println!("cargo:rustc-link-search={}", ld_dir.display());
+    }
+
+    // Linker scripts
+    {
+        println!("cargo:rustc-link-arg=-Tdefmt.x");
+        println!("cargo:rustc-link-arg=-Ttc37xA_memory.ld");
+    }
+
+    // Do not page align sections, link against static libraries
     println!("cargo:rustc-link-arg=--nmagic");
-    println!("cargo:rustc-link-arg=-Tdefmt.x");
-    println!("cargo:rustc-link-arg=-Ttc37xA_memory.ld");
 
     Ok(())
 }
